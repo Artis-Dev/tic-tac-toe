1e33136e776c4720abce50aa27612e460a84fe28
Artis-Dev <admin@moonart.lt>
2021-02-23 18:19:34 +0200

    Add basic layout, gameBoard module with basic logic, players factory

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-console.log('Hello World');
+// Gameboard module
+const gameBoard = (() => {
+  const board = ['', '', '', '', '', '', '', '', ''];
+
+  const gameContainer = document.querySelector('#game-container');
+
+  const renderBoard = () => {
+    for (let i = 0; i < board.length; i += 1) {
+      const gameDiv = document.createElement('div');
+      gameDiv.setAttribute('data-index', i);
+      gameContainer.appendChild(gameDiv);
+    }
+  };
+  renderBoard();
+
+  const gameDivs = document.querySelectorAll('#game-container > div');
+
+  const clickBoard = () => {
+    let lastPlayer = '';
+    gameDivs.forEach((element) => {
+      element.addEventListener('click', () => {
+        const index = element.getAttribute('data-index');
+        if (board[index] === '') {
+          if (lastPlayer === '' || lastPlayer === playerTwo) {
+            lastPlayer = playerOne;
+            board[index] = 'x';
+            lastPlayer.addMark(element, lastPlayer);
+          } else {
+            lastPlayer = playerTwo;
+            board[index] = 'o';
+            lastPlayer.addMark(element, lastPlayer);
+          }
+        } else {
+          console.log('Already taken');
+        }
+      });
+    });
+  };
+  clickBoard();
+
+  return {
+    board,
+    renderBoard,
+    clickBoard,
+  };
+})();
+
+// Players factory
+const players = (name) => {
+  const sayHello = () => console.log(`Hello! I'm ${name}`);
+  const addMark = (element, lastPlayer) => {
+    const el = element;
+    if (lastPlayer === playerOne) {
+      el.style.backgroundColor = '#00BBF9';
+    } else {
+      el.style.backgroundColor = '#F15BB5';
+    }
+    console.log(lastPlayer.name);
+  };
+  return { name, sayHello, addMark };
+};
+
+const playerOne = players('Artis');
+const playerTwo = players('moonii');
+playerOne.sayHello();
+playerTwo.sayHello();
