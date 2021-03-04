dd3a8394c97eb5777a83a8b951144c6c3373f80f
Artis-Dev <admin@moonart.lt>
2021-03-04 16:07:04 +0200

    Add create players UI, Font Awesome icons, start refactor JS

--- app.js
+++ app.js
@@ -1,79 +1,36 @@
-// Gameboard module
-const gameBoard = (() => {
-  const board = ['', '', '', '', '', '', '', '', ''];
-
-  const gameContainer = document.querySelector('#game-container');
-
-  const renderBoard = () => {
-    for (let i = 0; i < board.length; i += 1) {
-      const gameDiv = document.createElement('div');
-      gameDiv.setAttribute('data-index', i);
-      gameContainer.appendChild(gameDiv);
-    }
-  };
-  renderBoard();
-
-  const checkWinner = (player) => {
-    // ['x', 'x', 'x', '', '', '', '', '', '']
-    // ['', '', '', 'x', 'x', 'x', '', '', '']
-    // ['', '', '', '', '', '', 'x', 'x', 'x']
-    // ['x', '', '', 'x', '', '', 'x', '', '']
-    // ['', 'x', '', '', 'x', '', '', 'x', '']
-    // ['', '', 'x', '', '', 'x', '', '', 'x']
-    // ['x', '', '', '', 'x', '', '', '', 'x']
-    // ['', '', 'x', '', 'x', '', 'x', '', '']
-  };
-
-  const gameDivs = document.querySelectorAll('#game-container > div');
-
-  const clickBoard = () => {
-    let lastPlayer = '';
-    gameDivs.forEach((element) => {
-      element.addEventListener('click', () => {
-        const index = element.getAttribute('data-index');
-        if (board[index] === '') {
-          if (lastPlayer === '' || lastPlayer === playerTwo) {
-            lastPlayer = playerOne;
-            board[index] = 'x';
-            lastPlayer.addMark(element, lastPlayer);
-            checkWinner(lastPlayer);
-          } else {
-            lastPlayer = playerTwo;
-            board[index] = 'x';
-            lastPlayer.addMark(element, lastPlayer);
-            checkWinner(lastPlayer);
-          }
-        } else {
-          console.log('Already taken');
-        }
-      });
-    });
-  };
-  clickBoard();
-
-  return {
-    board,
-    renderBoard,
-    clickBoard,
-  };
-})();
-
 // Players factory
-const players = (name) => {
-  const sayHello = () => console.log(`Hello! I'm ${name}`);
-  const addMark = (element, lastPlayer) => {
-    const el = element;
-    if (lastPlayer === playerOne) {
-      el.style.backgroundColor = '#81B29A';
-    } else {
-      el.style.backgroundColor = '#E07A5F';
-    }
-    console.log(lastPlayer.name);
+const createPlayers = (defaultName, appearance, mark) => {
+  let name = defaultName;
+  const sayHello = () => console.log(`Hello! I'm ${appearance} ${name} with ${mark}`);
+  const setName = (newName) => {
+    name = newName;
   };
-  return { name, sayHello, addMark };
+  return {
+    name,
+    appearance,
+    mark,
+    sayHello,
+    setName,
+  };
 };
 
-const playerOne = players('Artis');
-const playerTwo = players('moonii');
-playerOne.sayHello();
-playerTwo.sayHello();
+const createGame = (() => {
+  const newGameModal = document.querySelector('#new-game-modal');
+  const startGameButton = document.querySelector('#start-game');
+
+  const playerOne = createPlayers('Player One', 'default', 'default');
+  const playerTwo = createPlayers('Player Two', 'default', 'default');
+
+  startGameButton.addEventListener('click', () => {
+    playerOne.setName('Artis');
+    playerTwo.setName('moonii');
+    playerOne.sayHello();
+    playerTwo.sayHello();
+    newGameModal.style.display = 'none';
+  });
+
+  return {
+    playerOne,
+    playerTwo,
+  };
+})();
